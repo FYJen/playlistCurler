2963967cac0ebcee087a1394e343a6eef20b64fd
Arthur Jen <cowboy5110@hotmail.com>
2014-09-18 17:56:11 -0400

    Add default config

---
+++ default_config.py
@@ -0,0 +1,16 @@
+# Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
+# tab of
+#   https://cloud.google.com/console
+# Please ensure that you have enabled the YouTube Data API for your project.
+DEVELOPER_KEY = 'AIzaSyBxa6WbBaQYJT6VBu0NKpGsD7e2qr_Ot-0'  # Apr 30, 2014 9:00 PM
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+
+# Default youtube video URL.
+YOUTUBE_VIDEO_URL = 'www.youtube.com/watch?v=%s'
+
+# Third party service that we will post our requests to.
+YOUTUBEINMP3 = 'http://youtubeinmp3.com/fetch/?video=%s'
+
+# Number of download workers.
+DOWNLOAD_WORKER_NUMBER = 3
